44aea19b082f46b001983fd165a87d1e942d0245
Tech2makemebuilder <77535283+Tech2makemebuilder@users.noreply.github.com>
2021-04-11 16:24:20 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,7 +1,7 @@
 <?php
-   $host ="localhost";
-   $user="root";
-   $password ="";
+   $host ="us-cdbr-east-03.cleardb.com";
+   $user="b8aa88ad6a27d3";
+   $password ="8e2ec6ec";
    $db_name ="mmb";
    $conn = mysqli_connect($host,$user,$password,$db_name);
  ?> 
