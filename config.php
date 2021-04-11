b88884e1c4cd3ab230d6e1ddda4673b5f994157d
Tech2makemebuilder <77535283+Tech2makemebuilder@users.noreply.github.com>
2021-04-11 16:31:32 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	$db_host='us-cdbr-east-03.cleardb.com';
 	$db_user='b8aa88ad6a27d3';
 	$db_pass='8e2ec6ec';
-	$db_name='mmb';
+	$db_name='heroku_8070bd2057ad505';
 
 	try {
 		$db_conn= new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
