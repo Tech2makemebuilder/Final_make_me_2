c2d78acc3fcaf81ba5e6ad87d717aa8cb82f09a5
Tech2makemebuilder <77535283+Tech2makemebuilder@users.noreply.github.com>
2021-04-11 16:44:53 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-$pdo = new PDO('mysql:host=us-cdbr-east-03.cleardb.com;port=3306;dbname=mmb', 'b8aa88ad6a27d3', '8e2ec6ec');
+$pdo = new PDO('mysql:host=us-cdbr-east-03.cleardb.com;port=3306;dbname=heroku_8070bd2057ad505', 'b8aa88ad6a27d3', '8e2ec6ec');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $ex)
